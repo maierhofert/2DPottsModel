a88385a10e1ddf071dc555c8662d4f1871f5b7c3
maierhofert <thomasjmaierhofer@gmail.com>
2019-06-14 10:11:28 -0700

    added Gibbs sampler GIF

--- animations.py
+++ animations.py
@@ -2,23 +2,23 @@
 
 def runIter_SW_missing(grid, miss, beta, n_states = 4):
     N = grid.shape[0]
-    # sample observed edges 
+    # sample observed edges
     # with probability 1 - exp(-beta)
     p = 1 - np.exp(-beta)
     # this a N by N by 4 (top, bottom, left right) array
     edges = np.random.choice(a=[True, False], size=(N, N, 4), p=[p, 1-p])
     # make sure edges are only between observations of the same class
-    # 
+    #
     edges[np.roll(a=grid, shift=1, axis=1) != grid, 0] = False
     # that would forbid clusters that contain observed values
     # edges[np.invert(np.roll(a=miss, shift=1, axis=1)), 0] = False
-    # 
+    #
     edges[np.roll(a=grid, shift=-1, axis=1) != grid, 1] = False
-    # 
+    #
     edges[np.roll(a=grid, shift=1, axis=0) != grid, 2] = False
-    # 
+    #
     edges[np.roll(a=grid, shift=-1, axis=0) != grid, 3] = False
-    
+
     # randomly initialize seed for cluster
     # only draw from missing values
     missing_coordinates = np.where(miss)
@@ -28,7 +28,7 @@ def runIter_SW_missing(grid, miss, beta, n_states = 4):
     # sample from the indices of missing observations
     loc = np.random.randint(missing_xyloc_array.__len__())
     xy_loc = missing_xyloc_array[loc]
-    
+
     # initialize cluster
     xylist = [xy_loc]
     reached_end = False
@@ -47,19 +47,19 @@ def runIter_SW_missing(grid, miss, beta, n_states = 4):
             new_xy = np.array([new_x%N, this_y])
             if not any((new_xy == loc).all() for loc in xylist):
               xylist.append(new_xy)
-          # 
+          #
           elif neighbor == 2:
             new_x = this_x - 1
             new_xy = np.array([new_x%N, this_y])
             if not any((new_xy == loc).all() for loc in xylist):
               xylist.append(new_xy)
-          # 
+          #
           elif neighbor == 1:
             new_y = this_y + 1
             new_xy = np.array([this_x, new_y%N])
             if not any((new_xy == loc).all() for loc in xylist):
               xylist.append(new_xy)
-          # 
+          #
           elif neighbor == 0:
             new_y = this_y - 1
             new_xy = np.array([this_x, new_y%N])
@@ -88,13 +88,13 @@ def runIter_SW_missing(grid, miss, beta, n_states = 4):
       idy = [xy[1] for xy in xylist]
       grid[idx, idy] = new_state
     return(grid)
-    
+
 #########################################################
 # # animations
 
 import numpy as np
 import matplotlib.colors as pltcols
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
 
@@ -137,7 +137,7 @@ grid[miss] = np.random.randint(n_states, size=miss.sum())
 # # animations
 def update(data):
     mat.set_data(data)
-    return mat 
+    return mat
 
 def data_gen():
     while True:
@@ -165,12 +165,12 @@ ani.save('plots/grid_missing_burnin.gif', dpi=200, writer='imagemagick')
 
 
 ####################################################################
-# run the update a bunch of time to get chain in equilibrium
+# run the update a bunch of times to get chain in equilibrium
 for i in range(1000):
   # est_beta = estimate_beta_MPL(grid=grid_est_beta, betas=np.arange(0.1, 1.5, 0.01), n_states=n_states)
   # estimate missing values given beta
   grid_est_beta = runIter_SW_missing(grid=grid_est_beta, miss=miss, beta=1)
-  
+
 
 # plot grid working with missing values
 ani = animation.FuncAnimation(fig, update, data_gen, interval=10, repeat_delay = 500,
@@ -184,12 +184,12 @@ ani.save('plots/grid_missing_equilibrium.gif', dpi=200, writer='imagemagick')
 # # SW in equlibrium without missing values
 def update(data):
     mat.set_data(data)
-    return mat 
+    return mat
 
 def data_gen():
     while True:
       global grid_beta
-      grid_beta = runIter_SW(grid=grid_beta, beta=1)
+      grid_beta = runIter_SW(grid=grid_beta, beta=1, n_states=4)
       yield grid_beta
 
 fig, ax = plt.subplots()
@@ -211,6 +211,57 @@ ani = animation.FuncAnimation(fig, update, data_gen, interval=10, repeat = True,
 # plt.show()
 
 ani.save('plots/grid_SW_equilibrium.gif', dpi=200, writer='imagemagick')
+
+# ###############################################################
+# # Gibbs sampler in equlibrium without missing values
+def runIter(grid, beta, n_states = 4):
+    N = grid.shape[0]
+    x, y = np.random.randint(0, N, 2)
+    # current point s
+    s = grid[x, y]
+    new_state = np.int64(np.random.randint(n_states))
+    # current energy state
+    neighbors = [grid[(x+1)%N, y], grid[x, (y+1)%N],
+          grid[(x-1)%N, y], grid[x, (y-1)%N]]
+    H = -sum(neighbors == s)
+    # new energy state
+    altH = -sum(neighbors == new_state)
+    #Compute dE: (H = -  \sum_{<i,j>} S_i S_j )
+    dE = altH - H 
+    if dE <= 0:
+        grid[x, y] = new_state
+    elif np.random.rand() < np.exp(-beta * dE): #high energy --> maybe flip?
+        grid[x, y] = new_state
+    return grid
+
+def update(data):
+    mat.set_data(data)
+    return mat 
+
+def data_gen():
+    while True:
+      global grid_beta
+      grid_beta = runIter(grid=grid_beta, beta=1, n_states=4)
+      yield grid_beta
+
+fig, ax = plt.subplots()
+    
+# set up Gibbs Sampler that estimates beta from data
+# then fills missing values using estimated beta
+grid_beta = grid.copy()
+
+for i in range(10000):
+  # estimate missing values given beta
+  grid_beta = runIter(grid=grid_beta, beta=1)
+
+
+mat = ax.matshow(grid_beta)
+# plt.show(mat)
+ani = animation.FuncAnimation(fig, update, data_gen, interval=5, repeat = True, repeat_delay = 500,
+                              save_count=500)
+# plt.show()
+
+ani.save('plots/grid_Gibbs_equilibrium.gif', dpi=200, writer='imagemagick')
 
 
 
